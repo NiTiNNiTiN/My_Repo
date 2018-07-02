def103a7c41ea8becf3f4de0dedda3b0e086e101
Nitin Kumar <nkumar@tillster.com>
2018-07-02 13:21:45 +0530

    Added src

---
+++ src/com/Main.java
@@ -0,0 +1,32 @@
+package com;
+
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        Robot robot = new Robot();
+
+        // SET THE MOUSE X Y POSITION
+        while (true) {
+            robot.mouseMove(600, 350);
+            robot.mousePress(InputEvent.BUTTON3_MASK);
+            robot.mouseRelease(InputEvent.BUTTON3_MASK);
+            Thread.sleep(5000);
+            robot.mouseMove(650, 300);
+            Thread.sleep(5000);
+            robot.mouseMove(400, 550);
+            robot.mousePress(InputEvent.BUTTON1_MASK);
+            robot.mouseRelease(InputEvent.BUTTON1_MASK);
+            Thread.sleep(5000);
+            robot.mouseMove(550, 300);
+            Thread.sleep(5000);
+            robot.mouseMove(400, 550);
+            Thread.sleep(5000);
+            robot.mouseMove(550, 300);
+
+        }
+    }
+}
